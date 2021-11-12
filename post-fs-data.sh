dbc351545dbfbc5e65cc0b1a95da31d3fcc28fa9
Cememer <cememer@icloud.com>
2021-11-12 23:57:24 +0900

    zramを3GBに増量

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,7 +2,7 @@
 
 echo lz4 > /sys/block/zram0/comp_algorithm
 echo 0 > /proc/sys/vm/page-cluster
-echo 2048M > /sys/block/zram0/disksize
+echo 3000M > /sys/block/zram0/disksize
 mkswap /dev/block/zram0
 swapon /dev/block/zram0
 
